09f3c57169199cb1a4aba91edc61fa4b3b7d5b23
zhiyuan <zyuaq@ust.hk>
2023-02-27 13:02:38 -0500

    eval

--- eval_s03_demo.sh
+++ eval_s03_demo.sh
@@ -1,22 +1,23 @@
 EXP=$1
 EPOCH=$2
-python -m torch.distributed.launch --nproc_per_node=1 run/validate_3d.py \
+PORT=$3
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT run/validate_3d.py \
     --cfg configs/chi3d/$EXP.yaml \
     --model_path output/$EXP/chi3d/multi_person_posenet_50/$EXP/checkpoint_$EPOCH.pth.tar \
     --out exp-out/$EXP-s03@$EPOCH
-python -m torch.distributed.launch --nproc_per_node=1 run/demo_3d.py \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT run/demo_3d.py \
     --cfg configs/demo/fight.yml \
     --model_path output/$EXP/chi3d/multi_person_posenet_50/$EXP/checkpoint_$EPOCH.pth.tar \
     --out exp-out/$EXP-fight@$EPOCH
-python -m torch.distributed.launch --nproc_per_node=1 run/demo_3d.py \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT run/demo_3d.py \
     --cfg configs/demo/crash.yml \
     --model_path output/$EXP/chi3d/multi_person_posenet_50/$EXP/checkpoint_$EPOCH.pth.tar \
     --out exp-out/$EXP-crash@$EPOCH
-python -m torch.distributed.launch --nproc_per_node=1 run/demo_3d.py \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT run/demo_3d.py \
     --cfg configs/demo/dance.yml \
     --model_path output/$EXP/chi3d/multi_person_posenet_50/$EXP/checkpoint_$EPOCH.pth.tar \
     --out exp-out/$EXP-dance@$EPOCH
-python -m torch.distributed.launch --nproc_per_node=1 run/demo_3d.py \
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=$PORT run/demo_3d.py \
     --cfg configs/demo/511.yml \
     --model_path output/$EXP/chi3d/multi_person_posenet_50/$EXP/checkpoint_$EPOCH.pth.tar \
     --out exp-out/$EXP-511@$EPOCH
